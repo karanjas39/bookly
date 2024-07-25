b5d1a569026d371bb91de3ff1cd202ce7091a174
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-07-25 10:26:02 +0530

    updated the schema

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,28 +5,23 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 
 const auth = new Hono();
 
-type signUpType = {
-  email: string;
-  name: string;
-};
-
 auth.post("/signup", async (ctx) => {
-  const body: signUpType = await ctx.req.parseBody();
+  const body = await ctx.req.parseBody();
 
   const { DATABASE_URL } = env<{ DATABASE_URL: string }>(ctx);
   const prisma = new PrismaClient({
     datasourceUrl: DATABASE_URL,
   }).$extends(withAccelerate());
 
-  const response = await prisma.user.create({
-    data: {
-      email: body.email,
-      name: body.name,
-    },
-  });
+  // const response = await prisma.user.create({
+  //   data: {
+  //     email: body.email,
+  //     name: body.name,
+  //   },
+  // });
 
   return ctx.json({
-    response,
+    response: "ok",
   });
 });
 
