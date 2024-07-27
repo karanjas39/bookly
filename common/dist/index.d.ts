ae64b8eb744a1c9f66ecc15afcc3dc22fe5f2a91
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-07-27 21:04:15 +0530

    changed the folder structure

---
+++ common/dist/index.d.ts
@@ -0,0 +1,110 @@
+import { z } from "zod";
+export declare const z_signUp: z.ZodObject<{
+    name: z.ZodString;
+    password: z.ZodString;
+    email: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    name: string;
+    password: string;
+    email: string;
+}, {
+    name: string;
+    password: string;
+    email: string;
+}>;
+export declare const z_singIn: z.ZodObject<{
+    password: z.ZodString;
+    email: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    password: string;
+    email: string;
+}, {
+    password: string;
+    email: string;
+}>;
+export declare const z_updatePassword: z.ZodObject<{
+    prevPassword: z.ZodString;
+    currPassword: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    prevPassword: string;
+    currPassword: string;
+}, {
+    prevPassword: string;
+    currPassword: string;
+}>;
+export declare const z_sellBook: z.ZodObject<{
+    name: z.ZodString;
+    description: z.ZodOptional<z.ZodString>;
+    author: z.ZodString;
+    price: z.ZodNumber;
+    genreId: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    name: string;
+    author: string;
+    price: number;
+    genreId: string;
+    description?: string | undefined;
+}, {
+    name: string;
+    author: string;
+    price: number;
+    genreId: string;
+    description?: string | undefined;
+}>;
+export declare const z_updateSellBook: z.ZodObject<{
+    id: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+    description: z.ZodOptional<z.ZodString>;
+    author: z.ZodOptional<z.ZodString>;
+    price: z.ZodOptional<z.ZodNumber>;
+    genreId: z.ZodOptional<z.ZodString>;
+    listed: z.ZodOptional<z.ZodBoolean>;
+}, "strip", z.ZodTypeAny, {
+    id: string;
+    name?: string | undefined;
+    description?: string | undefined;
+    author?: string | undefined;
+    price?: number | undefined;
+    genreId?: string | undefined;
+    listed?: boolean | undefined;
+}, {
+    id: string;
+    name?: string | undefined;
+    description?: string | undefined;
+    author?: string | undefined;
+    price?: number | undefined;
+    genreId?: string | undefined;
+    listed?: boolean | undefined;
+}>;
+export declare const z_id: z.ZodObject<{
+    id: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    id: string;
+}, {
+    id: string;
+}>;
+export declare const z_createFeedback: z.ZodObject<{
+    feedback: z.ZodString;
+    bookId: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    feedback: string;
+    bookId: string;
+}, {
+    feedback: string;
+    bookId: string;
+}>;
+export declare const z_createBuyRequest: z.ZodObject<{
+    bookId: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    bookId: string;
+}, {
+    bookId: string;
+}>;
+export type z_signUp_type = z.infer<typeof z_signUp>;
+export type z_singIn_type = z.infer<typeof z_singIn>;
+export type z_updatePassword_type = z.infer<typeof z_updatePassword>;
+export type z_sellBook_type = z.infer<typeof z_sellBook>;
+export type z_updateSellBook_type = z.infer<typeof z_updateSellBook>;
+export type z_id_type = z.infer<typeof z_id>;
+export type z_createFeedback_type = z.infer<typeof z_createFeedback>;
+export type z_createBuyRequest_type = z.infer<typeof z_createBuyRequest>;
---
+++ common/dist/index.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.z_createBuyRequest = exports.z_createFeedback = exports.z_id = exports.z_updateSellBook = exports.z_sellBook = exports.z_updatePassword = exports.z_singIn = exports.z_signUp = void 0;
+const zod_1 = require("zod");
+exports.z_signUp = zod_1.z.object({
+    name: zod_1.z.string(),
+    password: zod_1.z.string().min(6),
+    email: zod_1.z.string().email(),
+});
+exports.z_singIn = zod_1.z.object({
+    password: zod_1.z.string(),
+    email: zod_1.z.string().email(),
+});
+exports.z_updatePassword = zod_1.z.object({
+    prevPassword: zod_1.z.string().min(6),
+    currPassword: zod_1.z.string().min(6),
+});
+exports.z_sellBook = zod_1.z.object({
+    name: zod_1.z.string(),
+    description: zod_1.z.string().optional(),
+    author: zod_1.z.string(),
+    price: zod_1.z.number(),
+    genreId: zod_1.z.string().uuid(),
+});
+exports.z_updateSellBook = zod_1.z.object({
+    id: zod_1.z.string().uuid(),
+    name: zod_1.z.string().optional(),
+    description: zod_1.z.string().optional(),
+    author: zod_1.z.string().optional(),
+    price: zod_1.z.number().optional(),
+    genreId: zod_1.z.string().uuid().optional(),
+    listed: zod_1.z.boolean().optional(),
+});
+exports.z_id = zod_1.z.object({
+    id: zod_1.z.string().uuid(),
+});
+exports.z_createFeedback = zod_1.z.object({
+    feedback: zod_1.z.string(),
+    bookId: zod_1.z.string().uuid(),
+});
+exports.z_createBuyRequest = zod_1.z.object({
+    bookId: zod_1.z.string().uuid(),
+});
